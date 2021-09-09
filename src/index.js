5cf63aa78d19b18faaa6624d5741f448198f30c7
Ben Wilson <benjaminw1030@gmail.com>
2021-09-09 10:58:06 -0700

    refactor to use fetch

--- src/index.js
+++ src/index.js
@@ -2,12 +2,54 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Template from './js/template.js';
+import DndApi from './js/dnd.js';
 
-$(document).ready(function() {
-  $('#triangle-checker-form').submit(function(event) {
-    event.preventDefault();
+function clearFields() {
+  $('ul#spell-list').empty();
+  $('#spell-details').empty();
+}
 
-    $('#response').append("<p>" + response + "</p>");
+function attachContactListener(spell) {
+  $(`li#${spell.index}`).click(function () {
+    $('#spell-details').empty();
+    DndApi.spellDetails(spell.url)
+    .then (function(response) {
+      outputSpellDetails(response);
+    });
   });
+}
+
+function createSpellList(response) {
+  if (response.results) {
+    response.results.forEach(spell => {
+      $('ul#spell-list').append(`<li id="${spell.index}">${spell.name}</li>`);
+      attachContactListener(spell);
+    });
+  } else {
+    $('#error-output').text(`There was an error: ${response.message}`);
+  }
+}
+
+function outputSpellDetails(response) {
+  let components = response.components.join(", ");
+  $('div#spell-details').append(`<h5>${response.name}</h5>
+  <p>${response.desc[0]}</p>
+  <p>Casting time: ${response.casting_time}</p>
+  <p>Duration: ${response.duration}</p>
+  <p>Range: ${response.range}</p>
+  <p>Components: ${components}</p>
+  <p>School: ${response.school.name}</p>`);
+}
+
+$(document).ready(function () {
+  $('#spell-search').submit(function (event) {
+    event.preventDefault();
+    let level = $("#spell-level").val();
+    let school = $("#spell-school").val();
+    clearFields();
+    DndApi.searchSpell(level, school)
+      .then(function (response) {
+        createSpellList(response);
+      });
+  })
 });
--- src/js/dnd.js
+++ src/js/dnd.js
@@ -1,4 +1,27 @@
-export default class Template {
-  constructor() {
+export default class DndApi {
+  static searchSpell(level, school) {
+    return fetch(`https://www.dnd5eapi.co/api/spells?level=${level}&school=${school}`)
+      .then(function (response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function (error) {
+        return error;
+      })
+  }
+
+  static spellDetails(url) {
+    return fetch(`https://www.dnd5eapi.co${url}`)
+      .then(function (response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function (error) {
+        return error;
+      })
   }
 }
