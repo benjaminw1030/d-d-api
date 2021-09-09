f0a602e39d77c944ca3c6dcb113267ea5f2bd03e
Ben Wilson <benjaminw1030@gmail.com>
2021-09-09 15:02:14 -0700

    add monster search api, refactor

--- src/index.js
+++ src/index.js
@@ -5,40 +5,81 @@ import './css/styles.css';
 import DndApi from './js/dnd.js';
 
 function clearFields() {
-  $('ul#spell-list').empty();
-  $('#spell-details').empty();
+  $('ul#output-list').empty();
+  $('#output-details').empty();
 }
 
-function attachContactListener(spell) {
-  $(`li#${spell.index}`).click(function () {
-    $('#spell-details').empty();
-    DndApi.spellDetails(spell.url)
-    .then (function(response) {
-      outputSpellDetails(response);
-    });
+function attachContactListener(item) {
+  $(`li#${item.index}`).click(function () {
+    $('#output-details').empty();
+    DndApi.getDetails(item.url)
+      .then(function (response) {
+        $("ul#output-list > li.active").removeClass("active");
+        $(`li#${response.index}`).addClass("active");
+        if (response.school !== undefined) {
+          outputSpellDetails(response);
+        } else {
+          outputMonsterDetails(response);
+          console.log("monster")
+        }
+      });
   });
 }
 
-function createSpellList(response) {
+function createList(response) {
   if (response.results) {
-    response.results.forEach(spell => {
-      $('ul#spell-list').append(`<li id="${spell.index}">${spell.name}</li>`);
-      attachContactListener(spell);
+    response.results.forEach(item => {
+      $('ul#output-list').append(`<li class="list-group-item" id="${item.index}">${item.name}</li>`);
+      attachContactListener(item);
     });
   } else {
     $('#error-output').text(`There was an error: ${response.message}`);
   }
 }
 
+function assignOrdinal(n) {
+  let s = ["th", "st", "nd", "rd"];
+  let v = n % 100;
+  return n + (s[(v - 20) % 10] || s[v] || s[0])
+}
+
+function obtainClassNameList(classes) {
+  let classArray = []
+  classes.forEach(charClass => {
+    classArray.push(charClass.name);
+  });
+  return classArray.join(", ")
+}
+
 function outputSpellDetails(response) {
   let components = response.components.join(", ");
-  $('div#spell-details').append(`<h5>${response.name}</h5>
+  let level = assignOrdinal(response.level);
+  let subheaderLine = `${level} level ${response.school.name}`
+  let classes = obtainClassNameList(response.classes);
+  let durationLine = `Concentration, ${(response.duration).toLowerCase()}`
+  let subclassLine = ""
+  if (response.concentration === false) {
+    durationLine = response.duration;
+  }
+  if (response.ritual === true) {
+    subheaderLine += " (ritual)";
+  }
+  if (response.subclasses.length !== 0) {
+    subclassLine = `<p><b>Subclasses:</b> ${obtainClassNameList(response.subclasses)}</p>`;
+  }
+  $('div#output-details').append(`<h5>${response.name}</h5>
+  <p><em>${subheaderLine}</em></p>
+  <p><b>Casting Time:</b> ${response.casting_time}</p>
+  <p><b>Range:</b> ${response.range}</p>
+  <p><b>Components:</b> ${components}</p>
+  <p><b>Duration:</b> ${durationLine}</p>
   <p>${response.desc[0]}</p>
-  <p>Casting time: ${response.casting_time}</p>
-  <p>Duration: ${response.duration}</p>
-  <p>Range: ${response.range}</p>
-  <p>Components: ${components}</p>
-  <p>School: ${response.school.name}</p>`);
+  <p><b>Classes:</b> ${classes}</p>
+  ${subclassLine}`);
+}
+
+function outputMonsterDetails(response) {
+  $('div#output-details').append(`<h5>${response.name}</h5>`);
 }
 
 $(document).ready(function () {
@@ -49,7 +90,17 @@ $(document).ready(function () {
     clearFields();
     DndApi.searchSpell(level, school)
       .then(function (response) {
-        createSpellList(response);
+        createList(response);
+      });
+  })
+
+  $('#monster-search').submit(function (event) {
+    event.preventDefault();
+    let CR = $("#challenge-rating").val();
+    clearFields();
+    DndApi.searchMonster(CR)
+      .then(function (response) {
+        createList(response);
       });
   })
 });
--- src/js/dnd.js
+++ src/js/dnd.js
@@ -12,7 +12,20 @@ export default class DndApi {
       })
   }
 
-  static spellDetails(url) {
+  static searchMonster(CR) {
+    return fetch(`https://www.dnd5eapi.co/api/monsters?challenge_rating=${CR}`)
+      .then(function (response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function (error) {
+        return error;
+      })
+  }
+
+  static getDetails(url) {
     return fetch(`https://www.dnd5eapi.co${url}`)
       .then(function (response) {
         if (!response.ok) {
